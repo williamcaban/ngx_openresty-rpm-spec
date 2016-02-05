d93f7accd95d3064cb464d6e3934e96c1cfe6251
Lee Trout <lee@yhathq.com>
2016-02-05 10:53:39 -0500

    Add Makefile and unattended mode to build script

--- BUILD-and-INSTALL.sh
+++ BUILD-and-INSTALL.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 #
+HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+VERSION=1.9.7.2
+RELEASE=3
+USER=$(whoami)
+
+# Support unattended script run
+if [[ $1 == "go" ]]
+then
+	UNATTENDED=1
+else
+	UNATTENDED=0
+fi
+
 
 install_required_packages()
 {
 	echo -e "\nInstalling packages required to build RPMs...."
-	yum -y install epel-release
-	yum -y install git make gcc sed postgresql-devel readline-devel \
+	sudo yum -y install epel-release
+	sudo yum -y install git make gcc sed postgresql-devel readline-devel \
 	pcre-devel openssl-devel gcc pcre-devel libxml2-devel libxslt-devel \
 	gd-devel geoip-devel gperftools-devel libatomic_ops-devel rpm-build \
 	gperftools-devel lua-devel
@@ -13,32 +26,35 @@ install_required_packages()
 
 create_building_environment()
 {
-	echo -e "\nCreating directory structure and setting up SOURCES...." 
-	mkdir -p ~/rpmbuild/{SOURCES,SPECS}
-	cp ~/ngx_openresty-rpm-spec/SOURCES/ngx_openresty.service ~/rpmbuild/SOURCES/
-	#cd ~/rpmbuild/SOURCES && { curl -O https://openresty.org/download/ngx_openresty-1.9.3.1.tar.gz ; cd  -; }
-	if [ ! -f ~/ngx_openresty-rpm-spec/SOURCES/ngx_openresty-1.9.3.1.tar.gz ]; then
+	echo -e "\nCreating directory structure and setting up SOURCES...."
+	mkdir -p /home/${USER}/rpmbuild/{SOURCES,SPECS}
+	cp ${HERE}/SOURCES/ngx_openresty.service /home/${USER}/rpmbuild/SOURCES/
+	if [ ! -f ${HERE}/SOURCES/ngx_openresty-${VERSION}.tar.gz ]; then
 		echo -e "\nDownloading tar.gz source from openresty..."
-		curl -o ~/ngx_openresty-rpm-spec/SOURCES/ngx_openresty-1.9.3.1.tar.gz   https://openresty.org/download/ngx_openresty-1.9.3.1.tar.gz
+		curl -o ${HERE}/SOURCES/ngx_openresty-${VERSION}.tar.gz   https://openresty.org/download/ngx_openresty-${VERSION}.tar.gz
 	fi
-	cp ~/ngx_openresty-rpm-spec/SOURCES/ngx_openresty-1.9.3.1.tar.gz ~/rpmbuild/SOURCES/
-	cp ~/ngx_openresty-rpm-spec/SPECS/ngx_openresty.spec ~/rpmbuild/SPECS/
+	cp ${HERE}/SOURCES/ngx_openresty-${VERSION}.tar.gz /home/${USER}/rpmbuild/SOURCES/
+	cp ${HERE}/SPECS/ngx_openresty.spec /home/${USER}/rpmbuild/SPECS/
 }
 
 build_package()
 {
 	echo -e "\nBuilding package...."
-	rpmbuild -ba ~/rpmbuild/SPECS/ngx_openresty.spec
+	rpmbuild -ba /home/${USER}/rpmbuild/SPECS/ngx_openresty.spec
 }
 
 install_test_package()
 {
 	echo -e "\nInstalling package and dependencies...."
-	yum -y install /root/rpmbuild/RPMS/x86_64/ngx_openresty-1.9.3.1-1.el7.centos.x86_64.rpm
+	sudo yum -y install /home/${USER}/rpmbuild/RPMS/x86_64/ngx_openresty-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm
 }
 
+if [[ $UNATTENDED == 0 ]] ; then
+	read -n 1 -p "Install pre-req packages (y/n)?" yesno;
+else
+	yesno=y
+fi
 
-read -n 1 -p "Install pre-req packages (y/n)?" yesno;
 if [[ "$yesno" == "y" ]] ; then
  	install_required_packages
  	create_building_environment
@@ -47,8 +63,12 @@ else
     create_building_envioronment
 fi
 
-if [  -f ~/rpmbuild/SOURCES/ngx_openresty-1.9.3.1.tar.gz ]; then
-	read -n 1 -p "Build RPM packages (y/n)?" yesno;
+if [  -f /home/${USER}/rpmbuild/SOURCES/ngx_openresty-${VERSION}.tar.gz ]; then
+	if [[ $UNATTENDED == 0 ]] ; then
+		read -n 1 -p "Build RPM packages (y/n)?" yesno;
+	else
+		yesno=y
+	fi
 	if [[ "$yesno" == "y" ]] ; then
 	 	build_package
 	fi
@@ -56,8 +76,12 @@ else
 	echo -e "\nMissing dependency"
 fi
 
-if [ -f ~/rpmbuild/RPMS/x86_64/ngx_openresty-1.9.3.1-1.el7.centos.x86_64.rpm ]; then
-	read -n 1 -p "Install resulting RPM package (y/n)?" yesno;
+if [ -f /home/${USER}/rpmbuild/RPMS/x86_64/ngx_openresty-${VERSION}-${RELEASE}.el7.centos.x86_64.rpm ]; then
+	if [[ $UNATTENDED == 0 ]] ; then
+		read -n 1 -p "Install resulting RPM package (y/n)?" yesno;
+	else
+		yesno=y
+	fi
 	if [[ "$yesno" == "y" ]] ; then
 	 	install_test_package
 	fi
